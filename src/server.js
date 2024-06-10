1255f11545b3601b6f0b3a172510def3c658d4e9
Fabio <fabio@axicon.no>
2024-06-10 15:29:35 +0200

    Returns updated article

--- src/server.js
+++ src/server.js
@@ -34,7 +34,7 @@ app.put('/api/articles/:name/upvote', async (req, res) => {
   const article = await db.collection('articles').findOne({ name: name });
 
   if (article) {
-    res.send(`The article ${name} has now ${article.upvote} upvotes!`)
+    res.json(article);
   } else {
     res.send('That article does not exist!');
   }
