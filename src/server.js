c67bc261964fedd11953a10112e469f01ada4afc
Fabio <fabio@axicon.no>
2024-06-10 12:47:42 +0200

    Connects to rigth database and collection articles

--- src/server.js
+++ src/server.js
@@ -1,26 +1,65 @@
 import express from "express";
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import * as dotenv from 'dotenv'
+
+dotenv.config(); // Uses MONGODB_URI from .env file from my database connection om MongoDB 
 
 const app = express();
 app.use(express.json()); // Middleware to get req body in json format
 
+// let articlesInfo = [
+//   {
+//     name: "learn-react",
+//     upvote: 0,
+//     comments: [],
+//   },
+//   {
+//     name: "learn-node",
+//     upvote: 0,
+//     comments: [],
+//   },
+//   {
+//     name: "mongodb",
+//     upvote: 0,
+//     comments: [],
+//   },
+// ]
 
-let articlesInfo = [
-  {
-    name: "learn-react",
-    upvote: 0,
-    comments: [],
-  },
-  {
-    name: "learn-node",
-    upvote: 0,
-    comments: [],
-  },
-  {
-    name: "mongodb",
-    upvote: 0,
-    comments: [],
-  },
-]
+
+app.get('/api/articles/:name', async (req, res) => {
+  const { name } = req.params;
+  
+  // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+  const client = new MongoClient(process.env.MONGODB_URI, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    
+    const db = client.db('sample_mflix'); //Choose database to query
+    const article = await db.collection('articles').findOne({ name: name });
+
+    if(article){
+      res.json(article);
+    }else{
+      res.sendStatus(404);
+    }
+
+  } finally {
+    await client.close();
+  }
+
+})
+
 
 app.put('/api/articles/:name/upvote', (req, res) => {
   const { name } = req.params;
