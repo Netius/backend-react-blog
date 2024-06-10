043843d84c4299a84b725ca3c8ce4a83efb08b56
Fabio <fabio@axicon.no>
2024-06-10 13:17:34 +0200

    Adding comments to database

--- src/server.js
+++ src/server.js
@@ -78,16 +78,39 @@ app.put('/api/articles/:name/upvote', async (req, res) => {
   }
 })
 
-app.post('/api/articles/:name/comments', (req, res) => {
+app.post('/api/articles/:name/comments', async (req, res) => {
   const { name } = req.params;
   const { postedBy, text } = req.body;
-  const article = articlesInfo.find(article => article.name === name);
-  if (article) {
-    article.comments.push({ postedBy, text });
-    res.send(article.comments);
+  const client = new MongoClient(process.env.MONGODB_URI, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
 
-  } else {
-    res.send('That article does not exist!');
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    const db = client.db('sample_mflix'); //Choose database to query
+    await db.collection('articles').updateOne({ name: name }, {
+      $push:{comments: { postedBy, text }}
+    });
+
+    const article = await db.collection('articles').findOne({ name: name });
+   
+    if (article) {
+      res.send(article.comments);
+    } else {
+      res.send('That article does not exist!');
+    }
+ 
+  } finally {
+    await client.close();
   }
 })
 
